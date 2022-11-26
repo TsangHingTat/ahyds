8d3df7ce48da4f16c25dfa5109693d1885516a2b
TsangHingTat <hingtattsang@gmail.com>
2022-11-26 15:38:55 +0800

    update

--- M3 UItra/lesson/Views/lessonView.swift
+++ M3 UItra/lesson/Views/lessonView.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct lessonView: View {
+    @State var showbeta: Bool = true
     var body: some View {
-        JoinSessionView()
+        NavigationView {
+            List {
+                Text("Classroom is still under development, therefore some services may not work as expected and data may not be backward compatible.")
+                    .font(.title2)
+                    .foregroundColor(.red)
+                NavigationLink(destination: JoinSessionView().navigationBarBackButtonHidden(true)) {
+                    Text("Continue")
+                        .foregroundColor(.red)
+                }
+            }
+            .navigationTitle("Pre-alpha version")
+        }
+        
     }
 }
 
